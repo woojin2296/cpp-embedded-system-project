43dc8e60090d67b2060fa435a319aa72251a9ca7
NASUNJIN <74398946+NASUNJIN@users.noreply.github.com>
2021-12-10 15:52:11 +0900

    Update main.cpp
    주석 오류 수정

--- BusBackDoor/main.cpp
+++ BusBackDoor/main.cpp
@@ -19,22 +19,22 @@ const int D6 = 12; const int D7 = 13;
 
 LiquidCrystal lcd(RS, EN, D4, D5, D6, D7);
 
-// ��������
+// 서보모터
 Servo servo;
 
-// ������
+// 초음파
 int rangeMax = 200;
 int rangeMin = 0;
 
 long L;
 
 boolean dooropen = false;
-boolean openclose = false; // ���� ���ȴ� ����?
+boolean openclose = false; // 문이 열렸다 닫힘?
 boolean isClosed = false;
 
 unsigned long l1, l2;
 
-// �����ǥ��
+// 장애인 표시
 byte creat1[] = {
   B00000,
   B00000,
@@ -67,7 +67,7 @@ void setup() {
     pinMode(pinTrig, OUTPUT);
     pinMode(pinEcho, INPUT);
 
-    // ��������
+    // 서보모터
     servo.attach(pinServo);
     servo.write(180);
 
@@ -84,7 +84,7 @@ String strClose = "close";
 byte c = "1";
 
 void loop() {
-    // ������
+    // 초음파
     digitalWrite(pinTrig, LOW); delayMicroseconds(2);
     digitalWrite(pinTrig, HIGH); delayMicroseconds(10);
     digitalWrite(pinTrig, LOW);
@@ -92,12 +92,12 @@ void loop() {
     L = pulseIn(pinEcho, HIGH) / 58.82;
     Serial.println(L);
 
-    // �����ĺ��� ���ٴڱ��� �� 18cm
-    // �����ĺ��� ���ٴ�(��)���� �� 12cm
+    // 초음파부터 땅바닥까지 약 18cm
+    // 초음파부터 문바닥(끝)까지 약 12cm
 
-    // UART���(������-��ư)  �������� ��ư�� ������ �޹� ����
+    // UART통신(M:버튼 - S:뒷문) 마스터 버튼을 누르면 뒷문이 열림
     if (mySerial.available()) {
-        // Serial.readStringUntil('\n') \n���ڸ� ������ ���� ���ڿ� �б�
+        // Serial.readStringUntil('\n') \n문자를 만날때 까지 문자열 읽기
         String inString = mySerial.readStringUntil('\n');
         boolean open = inString.equals(strOpen);
         boolean close = inString.equals(strClose);
@@ -123,15 +123,15 @@ void loop() {
         }
     }
 
-    // ���(������-��ư) �����ķ� ���� ���� �������� ������ �����Ϳ� ���� ���ȴٰ� �˸�
+    // UART통신(M:버튼 - S:뒷문) 초음파로 인해 문열린 것 감지 > 마스터에 문열렀다고 알림
     if (isClosed && openclose && L <= 8) {
         servo.write(0);
         dooropen = true;
         openclose = false;
-        Serial.println("S - ������, open");
+        Serial.println("S - 문끼임, open");
         lcd.setCursor(0, 1); lcd.print("                ");
         lcd.setCursor(1, 1); lcd.print("CHECK BACKDOOR");
-        mySerial.write('1'); // ������ �߻��Ͽ� �������� close�� open���� �ٲ�
+        mySerial.write('1'); // 문끼임 발생하여 마스터쪽 close를 open으로 바꿈
     }
 
     l2 = millis();
@@ -141,7 +141,7 @@ void loop() {
 
 }
 
-// I2C��� (������-���̽�ƽ) ���� ������ ���� ���� LCD�� ���� ���
+// I2C통신(M:조이스틱 - S:뒷문 기사님 LCD) 버스 예약 후 버스 내부 LCD에 예약 출력
 void receiveEvent(int howMany) {
     char c = Wire.read();
 
